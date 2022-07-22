2ae25c5b83dc27ee83ba00797ef00d1910a5f900
Neuroth, Brad <neurothbr@epb.net>
2022-07-21 20:07:03 -0400

    handle errors

--- main.py
+++ main.py
@@ -11,19 +11,30 @@ VOL = None
 
 async def listen_wake():
     global VOL
-    async with websockets.connect('ws://10.0.0.110:12101/api/events/wake') as ws:
-        while True:
-            wake = await ws.recv()
-            VOL = await snapcast.cur_vol('radiopi')
-            await snapcast.volume_change('radiopi', 2, step_multiplier= 5)
+
+    try:
+        print("listening for wake")
+        async with websockets.connect('ws://10.0.0.110:12101/api/events/wake') as ws:
+            while True:
+                wake = await ws.recv()
+                VOL = await snapcast.cur_vol('radiopi')
+                await snapcast.volume_change('radiopi', 2, step_multiplier=5)
+    except websockets.ConnectionClosedError as e:
+        print(e)
+        start_wake_thread()
 
 
 async def listen_intent():
-    async with websockets.connect('ws://10.0.0.10:12101/api/events/intent') as ws:
-        while True:
-            data = await ws.recv()
-            jdata = json.loads(data)
-            await intent_switch(jdata)
+    try:
+        print("listening for intent")
+        async with websockets.connect('ws://10.0.0.10:12101/api/events/intent') as ws:
+            while True:
+                data = await ws.recv()
+                jdata = json.loads(data)
+                await intent_switch(jdata)
+    except websockets.ConnectionClosedError as e:
+        print(e)
+        start_intent_thread()
 
 
 async def intent_switch(jdata):
@@ -36,9 +47,16 @@ async def intent_switch(jdata):
     await snapcast.volume_change('radiopi', VOL)
 
 
-if __name__ == '__main__':
+def start_intent_thread():
     intent_thread = threading.Thread(target=asyncio.run, args=(listen_intent(),))
-    wake_thread = threading.Thread(target=asyncio.run, args=(listen_wake(),))
-
     intent_thread.start()
+
+
+def start_wake_thread():
+    wake_thread = threading.Thread(target=asyncio.run, args=(listen_wake(),))
     wake_thread.start()
+
+
+if __name__ == '__main__':
+    start_intent_thread()
+    start_wake_thread()
