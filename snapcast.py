03a08957811eed0afd354fe3e492f89319702b89
Neuroth, Brad <neurothbr@epb.net>
2022-07-29 15:03:20 -0400

    switch to using regular old requests... web socket was too slow

--- snapcast.py
+++ snapcast.py
@@ -1,9 +1,10 @@
 import asyncio
 import datetime
-import json
-import time
 
-import websockets
+import requests
+
+URL = 'http://10.0.0.10:1780/jsonrpc'
+HEADERS = {"content-type": "application/json"}
 
 
 def mac_from_device(device):
@@ -19,33 +20,28 @@ async def volume_change(device, end_vol, step_multiplier=1):
     mac = mac_from_device(device)
     current_vol = await cur_vol(device)
     vol_steps = []
-    async with websockets.connect('ws://10.0.0.10:1780/jsonrpc',
-                                  extra_headers={"content-type": "application/json"}) as ws:
-        direction = 1 if end_vol >= current_vol else -1
-        step = direction * step_multiplier
-        for x in range(current_vol, end_vol + direction, step):
-            await asyncio.sleep(.1)
-            if abs(x - end_vol) < abs(step):
-                x = end_vol
-            if x < 0 or x > 100:
-                print(f'Just tried to set the volume to {x}... aborting')
-                break
-            vol_data = {"id": 1337, "jsonrpc": "2.0", "method": "Client.SetVolume",
-                        "params": {"id": mac, "volume": {"muted": False, "percent": x}}}
-            vol_steps.append(x)
-            await ws.send(json.dumps(vol_data))
+
+    direction = 1 if end_vol >= current_vol else -1
+    step = direction * step_multiplier
+    for x in range(current_vol, end_vol + direction, step):
+        await asyncio.sleep(.1)
+        if abs(x - end_vol) < abs(step):
+            x = end_vol
+        if x < 0 or x > 100:
+            print(f'Just tried to set the volume to {x}... aborting')
+            break
+        vol_data = {"id": 1337, "jsonrpc": "2.0", "method": "Client.SetVolume",
+                    "params": {"id": mac, "volume": {"muted": False, "percent": x}}}
+        vol_steps.append(x)
+        resp = requests.post(URL, json=vol_data, headers=HEADERS)
+        print(resp.json())
+
     print(f'At {datetime.datetime.now()} I changed the volume in this order : {vol_steps}')
 
 
 async def cur_vol(device):
     mac = mac_from_device(device)
-    async with websockets.connect('ws://10.0.0.10:1780/jsonrpc',
-                                  extra_headers={"content-type": "application/json"}) as ws:
-        tick = int(time.time())
-        get_status_data = {"id": tick, "jsonrpc": "2.0", "method": "Client.GetStatus", "params": {"id": mac}}
-        await ws.send(json.dumps(get_status_data))
-        resp = await ws.recv()
-        jresp = json.loads(resp)
-        if jresp.get('id') == tick:
-            # this is our message response
-            return jresp.get('result').get('client').get('config').get('volume').get('percent')
+    get_status_data = {"id": 1337, "jsonrpc": "2.0", "method": "Client.GetStatus", "params": {"id": mac}}
+    resp = requests.post(URL, json=get_status_data, headers=HEADERS)
+    jresp = resp.json()
+    return jresp.get('result').get('client').get('config').get('volume').get('percent')
