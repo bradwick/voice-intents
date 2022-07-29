302945be432eac6cee466fca925d95e85af12995
Neuroth, Brad <neurothbr@epb.net>
2022-07-29 14:08:46 -0400

    throw a limiter on that bad boy

--- snapcast.py
+++ snapcast.py
@@ -9,6 +9,8 @@ import websockets
 def mac_from_device(device):
     if device == 'kitchen':
         return 'b8:27:eb:92:32:63'
+    elif device == 'laundry':
+        return 'c8:d3:ff:9e:56:93'
     else:
         return 'b8:27:eb:a6:6a:47'
 
@@ -25,6 +27,9 @@ async def volume_change(device, end_vol, step_multiplier=1):
             await asyncio.sleep(.1)
             if abs(x - end_vol) < abs(step):
                 x = end_vol
+            if x < 0 or x > 100:
+                print(f'Just tried to set the volume to {x}... aborting')
+                break
             vol_data = {"id": 1337, "jsonrpc": "2.0", "method": "Client.SetVolume",
                         "params": {"id": mac, "volume": {"muted": False, "percent": x}}}
             vol_steps.append(x)
